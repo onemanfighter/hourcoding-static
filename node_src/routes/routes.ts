72815ea9ebbf4a6d857443942e29259a0927bb9f
Amit Raikwar <managerarbusiness@gmail.com>
2024-12-13 19:34:48 +0530

    fix(): Fixing routes

--- node_src/routes/routes.ts
+++ node_src/routes/routes.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import serveIndex from "serve-index";
 
-const staticRoutes = express.static("node_src/static", {
+const staticRoutes = express.static("node_src/public", {
   cacheControl: true,
   maxAge: "30d",
   etag: true,
 });
-const serveStatic = serveIndex("node_src/static");
+const serveStatic = serveIndex("node_src/public");
 export { staticRoutes, serveStatic };
